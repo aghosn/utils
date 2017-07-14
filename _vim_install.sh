f0dc9271f6c55e69513130bdfcb432c4405b9f3e
Adrien Ghosn <ghosn.adrien@gmail.com>
2017-07-14 14:20:22 +0200

    Adding vice theme to config

--- _vim_install.sh
+++ _vim_install.sh
@@ -5,8 +5,12 @@ command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not install
 
 
 cd 
+mkdir .vim
+mkdir .vim/colors
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+curl -Lo ~/.vim/colors/vice.vim https://raw.githubusercontent.com/bcicen/vim-vice/master/colors/vice.vim
 
 wget https://raw.githubusercontent.com/aghosn/utils/master/vimrc
 mv vimrc .vimrc
