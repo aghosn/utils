b89b57cb4ea72409f86b5f2989ab10aa0b235609
Adrien Ghosn <ghosn.adrien@gmail.com>
2017-07-14 14:15:45 +0200

    correcting

--- _go_install.sh
+++ _go_install.sh
@@ -4,7 +4,7 @@ GO_ROOT="~/"
 GO_URL="https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz"
 GO_TAR="go1.8.3.linux-amd64.tar.gz"
 
-cd $GO_ROOT
+cd ~/
 pwd
 if [ -d "go" ]; then
 	echo -e "Error: the folder go already exists. Please remove it."
@@ -12,16 +12,16 @@ if [ -d "go" ]; then
 fi
 
 curl -O $GO_URL
-tar -xvf $GO_TAR
+tar -zxf $GO_TAR
 rm $GO_TAR
 
 if [ -d "Documents/Programs" ]; then
 	echo "Programs folder already exists."
 else 
-	mkdir Documents/Programs
+	mkdir ~/Documents/Programs
 fi
 
-mkdir Document/Programs/Go
+mkdir ~/Documents/Programs/Go
 
 
 #Setup the bashrc.
